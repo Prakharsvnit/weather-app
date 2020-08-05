400a4512376c3b4af21d9f1030b955b078856cea
Prakharsvnit <tprakhar468@gmail.com>
2020-08-05 13:09:52 +0530

    another commit!

--- server.js
+++ server.js
@@ -9,6 +9,4 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(5000,function(){
-	console.log('listening on port 5000');
-});
+app..listen(process.env.PORT, '0.0.0.0');
